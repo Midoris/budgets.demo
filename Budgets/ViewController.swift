2bf5c54b19d10fa3257e50e1f5dd79eb6e7f5935
Iablonskyi Ievgenii <eugen_yablonski@yahoo.com>
2019-01-07 07:39:36 +0100

    Initial Commit

---
+++ Budgets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Budgets
+//
+//  Created by IEVGENII IABLONSKYI on 07.01.19.
+//  Copyright © 2019 IEVGENII IABLONSKYI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

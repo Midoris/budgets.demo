48ab152fe02c7d3b5a0bf4755c40af3b83a192e5
Iablonskyi Ievgenii <eugen_yablonski@yahoo.com>
2019-01-30 20:09:23 +0100

    added storage manager

---
+++ Budgets/Common/Managers/BudgetsStorageManager.swift
@@ -0,0 +1,90 @@
+//
+//  BudgetsStorageManager.swift
+//  Budgets
+//
+//  Created by IEVGENII IABLONSKYI on 30.01.19.
+//  Copyright © 2019 IEVGENII IABLONSKYI. All rights reserved.
+//
+
+import Foundation
+
+struct BudgetsStorageManager {
+    
+    static func getBudgets(completion: ([Budget]) -> ()) {
+        
+        let salaryEntry = FundEntry(date: "20-12-2018".date(for: .littleEndian) ?? Date(),
+                                    amount: 3500.0,
+                                    description: ""
+        )
+        let salaryFund = Fund(
+            name: "Salary",
+            amount: 3500.0,
+            type: .income,
+            entries: [salaryEntry]
+        )
+        let apartmentEntry = FundEntry(date: "01-01-2019".date(for: .littleEndian) ?? Date(),
+                                       amount: -965.0,
+                                       description: ""
+        )
+        let apartmentFund = Fund(
+            name: "Apartment",
+            amount: 1100.0,
+            type: .recurring,
+            entries: [apartmentEntry]
+        )
+        let foodEntry1 = FundEntry(date: "05-01-2019".date(for: .littleEndian) ?? Date(),
+                                  amount: -51.1,
+                                  description: ""
+        )
+        let foodEntry2 = FundEntry(date: "07-01-2019".date(for: .littleEndian) ?? Date(),
+                                   amount: -70.1,
+                                   description: ""
+        )
+        let foodFund = Fund(
+            name: "Food",
+            amount: 300.0,
+            type: .normal,
+            entries: [foodEntry1, foodEntry2]
+        )
+        let stuffEntry = FundEntry(date: "09-01-2019".date(for: .littleEndian) ?? Date(),
+                                   amount: -15.0,
+                                   description: ""
+        )
+        let stuffFund = Fund(
+            name: "Clothes",
+            amount: 200.0,
+            type: .normal,
+            entries: [stuffEntry]
+        )
+        let baliEntry = FundEntry(date: "20-12-2018".date(for: .littleEndian) ?? Date(),
+                                  amount: -900.0,
+                                  description: ""
+        )
+        let baliFund = Fund(
+            name: "Bali",
+            amount: 900.0,
+            type: .saving,
+            entries: [baliEntry]
+        )
+        let investmentEntry = FundEntry(date: "20-12-2018".date(for: .littleEndian) ?? Date(),
+                                        amount: -1000,
+                                        description: ""
+        )
+        let investmentFund = Fund(
+            name: "Investments",
+            amount: 1000.0,
+            type: .saving,
+            entries: [investmentEntry]
+        )
+        let budget = Budget(
+            startDate: "20-12-2018".date(for: .littleEndian) ?? Date(),
+            endDate: "28-01-2019".date(for: .littleEndian) ?? Date(),
+            month: "Februrary",
+            incomeFunds: [salaryFund],
+            expensesFunds: [apartmentFund, foodFund, stuffFund, baliFund, investmentFund],
+            currencyCode: "EUR"
+        )
+        
+        completion([budget])
+    }
+}
